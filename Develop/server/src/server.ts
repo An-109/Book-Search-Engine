63bd4924db7219677a171c864f21aeb11e290cec
An-109 <doggod232471@gmail.com>
2025-01-16 21:49:11 -0700

    fixed the outdir

--- Develop/server/src/server.ts
+++ Develop/server/src/server.ts
@@ -35,10 +35,10 @@ const startApolloServer = async () => {
   }));
 
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/public')));
+    app.use(express.static(path.join(__dirname, '../client/dist')));
 
     app.get('*', (_req, res) => {
-      res.sendFile(path.join(__dirname, '../client/public/index.html'));
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
     });
   }
 
